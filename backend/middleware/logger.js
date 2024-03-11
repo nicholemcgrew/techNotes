be1b6cf61297b2b8106aaa3df92226e7f0a48cc0
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-11 12:50:24 -0500

    added user-auth

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
 
-const logEvents = async(message, logFileName) => {
-    const dateTime = `${format(new Date(), 'yyyyMMdd/tHH:mm:ss')}`
+const logEvents = async (message, logFileName) => {
+    const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
     try {
